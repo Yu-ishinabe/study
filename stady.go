edbda51626a9547472e266d046b319321f3b0aee
Yu-ishinabe <50506180+Yu-ishinabe@users.noreply.github.com>
2020-04-15 17:09:59 +0900

    型変換、エラー、スライス、マップ、構造体の追加

--- stady.go
+++ stady.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 )
 
 const count int = 10
@@ -12,11 +14,18 @@ var (
 	plusAlias = plus
 )
 
+// type で Servant型を定義 何回も同じ内容を利用するなら type記載
+type Servant = struct {
+	name  string
+	class string
+}
+
 func main() {
 	// 配列確認
-	a := [2][2]int{{1, 2}, {3, 4}}
+	a := [2][2]int{{1, 2}, {3, 4}} // 文字列の場合 [2][2]string{"a", "b"}, {"c", "d"}
 	b := 5
 	fmt.Println(a)
+
 	// if確認
 	if a[0][1] > b {
 		fmt.Println("大きい", a[0][1])
@@ -62,7 +71,61 @@ func main() {
 	fmt.Println(l)
 
 	// 関数を返す関数
-	returnAsk()() // 戻り値を変数に入れなくても直接呼び出せる
+	returnAsk()() // 戻り値を変数に入れなくても直接呼び出せる。最後の()で出力
+
+	//  型変換とエラー処理
+	str := "10"                 // "a"にするとintにはならないのでエラーになる
+	i, err := strconv.Atoi(str) //stringからintへ (strconvが変換するパッケージ)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(i) // float <=> int、string <=> []byte は変換可能
+
+	// スライス 配列の一部を切り出す構造体
+	m := [...]int{1, 2, 3, 4, 5}
+	m2 := m[1:4] //mの配列内の1~3を参照している (最後の4は4番目までではなく「その前まで」になる)
+	fmt.Println(m)
+	fmt.Println(m2)
+
+	m2[0] = 10     // スライスは元の配列を参照しているだけで、データを持っていない。
+	fmt.Println(m) // m2[0]の処理のスライスを通して、元の配列も変わっている
+	fmt.Println(m2)
+
+	// マップ キーと値をマッピングさせている構造体
+	n := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+	fmt.Println(n)
+	delete(n, "b") // キーn bの要素の削除
+	fmt.Println(n)
+	res, has := n["c"] // 要素にアクセスした場合２つ戻り値を受けてれて、1つ目は値が入り、2つ目は要素が存在しているかのbool型による判定
+	if has {
+		fmt.Printf("%#+v\n", res)
+	} else {
+		fmt.Println("has not")
+	}
+
+	// 構造体
+	servant := Servant{
+		// Servant型を フィールド名:値 で初期化をする
+		name:  "Altria",
+		class: "Saber",
+	}
+	/* 一時的な場合はtypeではなく、直接以下のようにも書ける
+	var servant struct {
+		name  string
+		class string
+	}{
+		name:  "Altria",
+		class: "Saber",
+	}
+	*/
+
+	// 「.」でフィールドにアクセス可能
+	fmt.Printf("name: %s, class: %s\n", servant.name, servant.class)
+
+	// 値の代入も「.」で可能
+	servant.name = "Emiya"
+	servant.class = "Archer"
+	fmt.Printf("name: %s, class: %s\n", servant.name, servant.class)
 }
 
 func plus(a, b int) int {
