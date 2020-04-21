8ed3f2df6a62c470e071296458cbf8ae7fbb6f2f
Yu-ishinabe <50506180+Yu-ishinabe@users.noreply.github.com>
2020-04-21 13:58:05 +0900

    error追加 直書きhtml

--- webApp/main.go
+++ webApp/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "test message")
+	fmt.Fprintln(w, "<!DOCTYPE html>")
+	fmt.Fprintln(w, "<html lang='ja'>")
+	fmt.Fprintln(w, "<head>")
+	fmt.Fprintln(w, "<meta charset='UTF-8'>")
+	fmt.Fprintln(w, "<title>TITLE</title>")
+	fmt.Fprintln(w, "</head>")
+	fmt.Fprintln(w, "<body>")
+	fmt.Fprintln(w, "<h1>hello GO!</h1>")
+	fmt.Fprintln(w, "</body>")
+	fmt.Fprintln(w, "</html>")
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil): err != nil {
+		log.Fatal("ListenAndServe failed.", err)
+	}
 }
