e309eb84c9483b61292abb2895cc784ce09247c5
Yu-ishinabe <50506180+Yu-ishinabe@users.noreply.github.com>
2020-04-22 12:22:52 +0900

    css読み込み

--- webApp/main.go
+++ webApp/main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("sample.html")
+	t, _ := template.ParseFiles("template/sample.html")
 	t.Execute(w, nil)
 }
 
 func main() {
+	// cssの読込
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
 	http.HandleFunc("/sample", home)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe failed.", err)
